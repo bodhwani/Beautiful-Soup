e7db2f4b2449f6e24a72b5b4f1a21b8ed2497bfa
Vinit Bodhwani <vinit.bodhwani2015@vit.ac.in>
2017-03-11 11:25:13 +0530

    Grabing soup using lxml

---
+++ delete1.py
@@ -0,0 +1,6 @@
+import bs4 as bs
+import urllib.request
+
+sauce = urllib.request.urlopen('https://pythonprogramming.net/parsememcparseface/').read()
+soup = bs.BeautifulSoup(sauce, 'lxml')
+print(soup)
