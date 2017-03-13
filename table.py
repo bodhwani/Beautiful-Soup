c7ebc79e8250c118bf17741da7b1bbd2da065de6
Vinit Bodhwani <vinit.bodhwani2015@vit.ac.in>
2017-03-13 21:21:27 +0530

    Scraping Tables

---
+++ table.py
@@ -0,0 +1,18 @@
+import bs4 as bs
+import urllib.request
+
+sauce = urllib.request.urlopen('https://pythonprogramming.net/parsememcparseface/').read()
+soup = bs.BeautifulSoup(sauce, 'lxml')
+#print(soup)
+
+table = soup.table
+#table = soup.find('table')
+#These are the same things
+#print(table)
+
+table_rows = table.find_all('tr')
+
+for tr in table_rows:
+    td = tr.find_all('td')
+    row = [i.text for i in td]
+    print(row)
